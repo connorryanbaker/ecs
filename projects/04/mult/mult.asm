817fd799dd7f848e8f82024f660b6abb7aa461e8
connorryanbaker <connorbaker@gmail.com>
2019-05-21 08:42:14 -0400

    add multiplication asm program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,20 +1,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-	@0 //sets A Register to value = R[0]
-	D=M //sets D Register to A register
-(LOOP)
-	D=M+M //sets D Register to D + D
-	@2 //sets A Register to value of R[2]
-	M=D //sets A Register to D Register
-	@1 //sets A Register to R[1]
-	M=M-1 //Decrements A Register by 1
+	@2 // store val of R2 in A register
+	M=0 // set it to 0
+	@c // initialize counter variable
+	M=0 // set it to 0
+    (LOOP)
+	@c // set A reg to counter
+	D=M // set D reg to counter
+	@0 // set A reg to R0
+	D=D-M // D=c-R0 (this will likely be negative initially)
 	@END
-	D;JGT
-	@r2
-	M=M+R0
-	@LOOP
-	0;JMP
-(END)
-	@END
-	0;JMP       
+	D;JGE // if D (i - R0 >= 0) goto END
 	
+	@1 // set A reg to val of R1
+	D=M // set D reg to val of R1
+	@2 // set A reg to val of R2
+	M=D+M // set A reg to val of R2+R1
+	@c
+	M=M+1 // increment counter
+	@LOOP
+	0;JMP // loop
+    (END)
+	@END
+	0;JMP
+		
